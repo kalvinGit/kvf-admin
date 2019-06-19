c173f1d1711d998dc504fb2e4aa55a2bd34ecd1c
linkaihui <linkaihui2012@sina.com>
2019-06-19 09:59:06 +0800

    解决jq获取url参数中文乱码问题

--- src/main/resources/static/js/kcommon.js
+++ src/main/resources/static/js/kcommon.js
@@ -203,7 +203,8 @@ var kvfKit = {
      */
     getUrlParam: function (name) {
         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-        var r = window.location.search.substr(1).match(reg);
+        var l = decodeURI(window.location.search);  // 解决中文乱码问题
+        var r = l.substr(1).match(reg);
         if (r != null) return unescape(r[2]);
         return null;
     },
